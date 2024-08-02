7f65c6fdfe259ebaa8484d2ad209807a11c7fdcc
Samuel Egan <samuel.egan@hswt.de>
2024-08-02 10:59:36 +0200

    fix

--- R/std.ramp.R
+++ R/std.ramp.R
@@ -23,9 +23,9 @@ std.ramp.lin <- function(x,x1,x2){return(
 )}
 
 # Quadratic 
-std.ramp.quad <- function(x, x1, x2, k, m) {
+std.ramp.quad <- function(x, x1, x2, k) {
   linear_part <- (x - x1) / (x2 - x1)
-  quadratic_part <- m*(linear_part^k)
+  quadratic_part <- linear_part^k
   return(
     apply(
       as.matrix(
