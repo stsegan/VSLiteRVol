2ae0504673d772b5d0f4a6661f8cf7aecf05b9ad
Samuel Egan <samuel.egan@hswt.de>
2025-02-05 10:21:41 +0100

    Linear -> Quadratic.

--- R/VSLite.R
+++ R/VSLite.R
@@ -81,10 +81,10 @@ VSLite <- function(syear, eyear, phi, Te, Pr, k, m,
   # Make sure here to add/remove k & m depending on ramp type (for both gT & gM). 
   
   # Temperature growth response:
-  gT <- std.ramp.lin(Te, T1, T2, k)
+  gT <- std.ramp.quad(Te, T1, T2, k, m)
   
   # Soil moisture growth response:
-  gM <- std.ramp.lin(M, M1, M2, k)
+  gM <- std.ramp.quad(M, M1, M2, k, m)
   
   # Compute overall growth rate:
   Gr <- kronecker(matrix(1,1,nyrs),gE)*pmin(gT,gM)
